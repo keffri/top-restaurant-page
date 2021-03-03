d396fb09d446cdb6f8f4b69e246d75bba161fe69
keffri <keffri.w.neal@gmail.com>
2021-03-02 18:57:29 -0800

    home js function

---
+++ src/modules/home.js
@@ -0,0 +1,43 @@
+import createHeader from "./header";
+import createFooter from "./footer";
+
+function createHomeMain(type, id) {
+  const homeMain = document.createElement(type);
+  homeMain.setAttribute("id", id);
+
+  const homeTitle = document.createElement("h1");
+  homeTitle.setAttribute("id", "homeTitle");
+  homeTitle.textContent = "Lorem Lounge";
+
+  const homeText = document.createElement("p");
+  homeText.setAttribute("id", "homeText");
+  homeText.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quo sequi, porro nemo, quam fugiat magnam nisi quae itaque consequuntur aliquid provident aperiam temporibus velit officia assumenda, est accusamus illo!";
+
+  homeMain.appendChild(homeTitle);
+  homeMain.appendChild(homeText);
+
+  return homeMain;
+}
+
+function createHomeContainer(type, id) {
+  const homeContainer = document.createElement(type);
+  homeContainer.setAttribute("id", id);
+  return homeContainer;
+}
+
+function home() {
+  const contentContainer = document.getElementById("content");
+  const homeContainer = createHomeContainer("div", "homeContainer");
+  const header = createHeader("header", "header");
+  const homeMain = createHomeMain("main", "homeMain");
+  const footer = createFooter("footer", "footer");
+
+  homeContainer.appendChild(header);
+  homeContainer.appendChild(homeMain);
+  homeContainer.appendChild(footer);
+
+  contentContainer.appendChild(homeContainer);
+}
+
+export default home;

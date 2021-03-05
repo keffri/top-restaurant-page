735a08a55e70ba7188eee352ecf60ceb24046da5
keffri <keffri.w.neal@gmail.com>
2021-03-04 17:48:35 -0800

    added init + event listeners

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import home from "./modules/home";
 import menu from "./modules/menu";
 
-// home();
-menu();
+const content = document.getElementById("content");
+
+function createButtons() {
+  const homeButton = document.getElementById("home");
+  homeButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    home();
+    createButtons();
+  });
+
+  const menuButton = document.getElementById("menu");
+  menuButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    menu();
+    createButtons();
+  });
+
+  const infoButton = document.getElementById("info");
+  infoButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    info();
+    createButtons();
+  });
+}
+
+function init() {
+  home();
+  createButtons();
+}
+
+init();
